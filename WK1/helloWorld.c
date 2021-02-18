c1afcd7142a44b829fa7fa9874ddfcfa372ca38d
IbrahemElsawy <79288021+IbrahemElsawy@users.noreply.github.com>
2021-02-18 18:12:59 -0500

    Create helloWorld.c

---
+++ WK1/helloWorld.c
@@ -0,0 +1,21 @@
+/* 
+Student Name: Ibrahem Elsawy 
+Student Number: 157888199
+Student Email: ielsawy@myseneca.ca
+*/
+
+/* This is Ibrahem's helloWorld. */
+
+#include <stdio.h>    // It provides the input/output facilities in the C program.
+
+int main()      //  The main () is to start the function of the program.
+
+{               // This is to start the body of the program.
+
+    printf("Hello, World!\n");     // The output greeting.
+
+    printf("This is Ibrahem.\n"); // The output of my name.
+
+    return 0; // This is to return the codes back to the operating system (OS).
+
+} // This is used to end the body of the program.
