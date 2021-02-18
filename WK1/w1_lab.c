d4de3e5ab6e0134aa7590434df5edc43a45493ca
Ibrahem Elsawy <79288021+IbrahemElsawy@users.noreply.github.com>
2021-02-18 18:17:55 -0500

    Create w1_lab.c

---
+++ WK1/w1_lab.c
@@ -0,0 +1,24 @@
+/*
+  Name: Ibrahem Elsawy
+  Student Number: 157888199
+  Student Email: ielsawy@myseneca.ca
+
+*/
+
+/* Ibrahem's Workshop 1 At Lab Assignment */
+
+#include <stdio.h> // It provides the input/output facilities in the C program.
+
+int main(void) // The main () is to start the function of the program.
+
+{ // This is to start the body of the program.
+
+	printf("*** Welcome to C Programming ***\n"); //This function is to print a string that says, "*** Welcome to C Programming ***."
+
+	printf("\n"); // This is to create a new line that is blank so it is easier to see the above printed string and the below printed string.
+
+	printf("Press any key to exit the program: \n"); // This function is to print a string  saying, "Press any key to exit the program:."
+
+	return (0); // This is to return the codes back to the operating system (OS).
+
+} // This is used to end the body of the program.
